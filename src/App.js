4be684490a4f1785454394d0d490be219ecd26db
zatkiller <gheedylan@yahoo.com.sg>
2020-07-11 06:54:00 +0800

    Added FaceRecognition Component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank.js";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import "./App.css";
+
+const app = new Clarifai.App({
+	apiKey: "ab176e370e6c4706b3be0b91b53f1524",
+});
 
 const particlesOptions = {
 	particles: {
@@ -22,16 +28,29 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			input: ",",
+			input: "",
+			imageUrl: "",
 		};
 	}
 
 	onInputChange = (event) => {
-		console.log(event.target.value);
+		this.setState({ input: event.target.value });
 	};
 
 	onButtonSubmit = () => {
+		this.setState({ imageUrl: this.state.input });
 		console.log("click");
+		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+			//Cannot use this.stata.imageUrl due to setState
+			function (response) {
+				console.log(
+					response.outputs[0].data.regions[0].region_info.bounding_box
+				);
+			},
+			function (err) {
+				// there was an error
+			}
+		);
 	};
 
 	render() {
@@ -45,7 +64,7 @@ class App extends Component {
 					onInputChange={this.onInputChange}
 					onButtonSubmit={this.onButtonSubmit}
 				/>
-				{/* <FaceRecognition/> */}
+				<FaceRecognition imageUrl={this.state.imageUrl} />
 			</div>
 		);
 	}
