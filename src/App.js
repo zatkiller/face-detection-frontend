13ce4e33bd4f51d36d35f71625c89bbc5e5b3355
zatkiller <gheedylan@yahoo.com.sg>
2020-07-11 17:38:25 +0800

    Added Face Box

--- src/App.js
+++ src/App.js
@@ -30,8 +30,28 @@ class App extends Component {
 		this.state = {
 			input: "",
 			imageUrl: "",
+			box: {},
 		};
 	}
+
+	calculateFaceLocation = (data) => {
+		const clarifaiFace =
+			data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById("inputimage"); //DOM Manipulation
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height,
+		};
+	};
+
+	displayFaceBox = (box) => {
+		console.log(box);
+		this.setState({ box: box });
+	};
 
 	onInputChange = (event) => {
 		this.setState({ input: event.target.value });
@@ -40,17 +60,12 @@ class App extends Component {
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
 		console.log("click");
-		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-			//Cannot use this.stata.imageUrl due to setState
-			function (response) {
-				console.log(
-					response.outputs[0].data.regions[0].region_info.bounding_box
-				);
-			},
-			function (err) {
-				// there was an error
-			}
-		);
+		app.models
+			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input) //Cannot use this.stata.imageUrl due to setState
+			.then((response) =>
+				this.displayFaceBox(this.calculateFaceLocation(response))
+			)
+			.catch((err) => console.log(err));
 	};
 
 	render() {
@@ -64,7 +79,10 @@ class App extends Component {
 					onInputChange={this.onInputChange}
 					onButtonSubmit={this.onButtonSubmit}
 				/>
-				<FaceRecognition imageUrl={this.state.imageUrl} />
+				<FaceRecognition
+					box={this.state.box}
+					imageUrl={this.state.imageUrl}
+				/>
 			</div>
 		);
 	}
