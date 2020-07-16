dc22c70d7663e7e8494ff0ae5c860d3c09a90499
zatkiller <59414467+zatkiller@users.noreply.github.com>
2020-07-16 18:05:38 +0800

    Fix entry count

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
 			.then((response) => response.json())
 			.then((response) => {
 				if (response) {
-					fetch("https://stark-peak-06534.herokuapp.com:3000/image", {
+					fetch("https://stark-peak-06534.herokuapp.com/image", {
 						method: "put",
 						headers: { "Content-Type": "application/json" },
 						body: JSON.stringify({
